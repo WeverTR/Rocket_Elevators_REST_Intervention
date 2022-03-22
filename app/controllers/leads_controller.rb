e5cdd1ba92458582af65d9191411de762d80c630
Digby Laret <dlaret99@gmail.com>
2022-03-22 16:06:20 -0400

    sendgrid1

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,8 +1,7 @@
 class LeadsController < ApplicationController
     skip_before_action :verify_authenticity_token
-    
     def create
-        lead = Lead.create!(
+        @lead = Lead.create!(
             contact_name: params[:contact_name],
             company_name: params[:company_name],
             email: params[:email],
@@ -15,7 +14,6 @@ class LeadsController < ApplicationController
         )
 
         redirect_to('/index')
-
     end
 end
 
