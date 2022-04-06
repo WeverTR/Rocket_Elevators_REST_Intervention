24beb10c52e73e2b5f5f6a0986028ff52b2faaff
weverTR <troywever@gmail.com>
2022-04-06 12:43:58 -0400

    Revert "Form almost done, need to get author and employee ID to persist"
    This reverts commit 2d361704a50b71b210b32520b9383d6f7e3b990e.

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   # get 'quotes_controller/create'
   post '/quote', to: 'quotes#create'
 
-  post '/lead', to: 'leads#create'
 
-  post '/intervention', to: 'interventions#create'
+
+  post '/lead', to: 'leads#create'
 
   #route to home page, commercial,residential, and quote
   root to: 'pages#index'
