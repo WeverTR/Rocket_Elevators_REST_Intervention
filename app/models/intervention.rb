123b25486b4318a4b9a1a06caca00117f611a327
weverTR <troywever@gmail.com>
2022-04-13 12:13:55 -0400

    More fixes

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -1,8 +1,8 @@
 class Intervention < ApplicationRecord
-    belongs_to :author, class_name: 'Employee', optional: true
-    belongs_to :customer, optional: true
-    belongs_to :building, optional: true
-    belongs_to :battery, optional: true
+    belongs_to :author, class_name: 'Employee'
+    belongs_to :customer
+    belongs_to :building
+    belongs_to :battery
     belongs_to :column, optional: true
     belongs_to :elevator, optional: true
     belongs_to :employee, optional: true
